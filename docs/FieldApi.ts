8c4f73dd30f3cb763e71ae961cd21a33d8997d25
tianyanqiu <745916729@qq.com>
2020-08-26 11:29:32 +0800

    docs: 添加表单状态管理的文档

---
+++ docs/FieldApi.ts
@@ -0,0 +1,63 @@
+import { FieldValidateProps, FieldModel } from '../src/types';
+
+interface FieldConfig<T = any> extends FieldValidateProps {
+  /**
+   * 表单域名称。可以指定路径。
+   *
+   * 例如：
+   *
+   * * `userName`
+   * * `address.city`
+   * * `friends[0].name`
+   */
+  name: string;
+  /**
+   * 指定表单域组件
+   */
+  as?: React.ReactType;
+  /**
+   * 默认值。
+   */
+  defaultValue?: T;
+  /**
+   * 指定值提取器
+   */
+  valueExtract?: (event: any, ...rest: any[]) => T;
+
+  /**
+   * 表单域异步校验方法
+   *
+   *
+   * @returns {(string | undefined | null | Promise<string | undefined>} 返回校验结果
+   */
+  asyncValidate?: (value: any, values: any) => Promise<string | undefined>;
+
+  /**
+   * 关联字段名
+   */
+  relyFields?: string[];
+
+  /**
+   * 值关联计算方法
+   * @param newValues 表单新的值
+   * @param oldValues 表单旧的值
+   */
+  relyFn?: (newValues: T, oldValues: T) => void;
+
+  /**
+   * 指定渲染函数
+   */
+  render?: (props: FieldModel<T>) => React.ReactNode;
+
+  /**
+   * 引用as组件
+   */
+  innerRef?: React.Ref<any>;
+}
+
+function Field<Value>(props: FieldConfig<Value>) {
+  console.log(props);
+  return null;
+}
+
+export default Field;
